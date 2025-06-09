87c51de6627ac28408d65949b8a8ebb6d67ab2bb
jan wiener <jan.wiener.024@pslib.cz>
2025-06-09 20:05:51 +0200

    test+joker texture

---
+++ run_test.ps1
@@ -0,0 +1,7 @@
+
+
+Invoke-Expression "cmd /c start powershell -Command {py -3.13 client_new.py;}"
+Invoke-Expression "cmd /c start powershell -Command {py -3.13 client_new.py;}"
+Invoke-Expression "cmd /c start powershell -Command {py -3.13 client_new.py;}"
+py -3.13 main.py
+exit
